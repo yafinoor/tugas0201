710f4e3dace893a06f802be5d888f15d8dd23a15
yafinoor <mnyafi777@gmail.com>
2017-10-23 12:40:18 +0700

    menambahkan 4 variable bertipe data double

--- tugas0204.cpp
+++ tugas0204.cpp
@@ -5,6 +5,9 @@ int main(){
 	double luasRumah = 42;
 	double lantaiParket = 19*1215;
 	double luas1box = lantaiParket*8/1000000;
+	
 	cout << 'Luas Rumah	:'<<luasRumah<<"m2"<<endl;
 	cout << 'Luas 1 box parket :'<<luas1box<<"m2"<<endl;
+	
+	double jumlahBoxP,biayaParket,biayaPasang,totalBiaya;
 }
